b67f264ac2871843a7d492bd2073c3c282c249a4
Anup D'Souza <anup.dsouza@gmail.com>
2024-08-28 18:23:11 +0530

    Update UI & socials
    Added pokemon images & app icon

--- ScratchCard/ScratchCard/ScratchCardCanvasMaskView.swift
+++ ScratchCard/ScratchCard/ScratchCardCanvasMaskView.swift
@@ -3,6 +3,11 @@
 //  ScratchCard
 //
 //  Created by Anup D'Souza on 16/11/23.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/adsouza
+//  🫶🏼 https://patreon.com/adsouza
 //
 
 import SwiftUI
--- ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
+++ ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
@@ -3,6 +3,11 @@
 //  ScratchCard
 //
 //  Created by Anup D'Souza on 16/11/23.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/adsouza
+//  🫶🏼 https://patreon.com/adsouza
 //
 
 import SwiftUI
@@ -11,34 +16,53 @@ struct ScratchCardPathMaskView: View {
     @State var points = [CGPoint]()
     
     var body: some View {
-        ZStack {
-            // MARK: Scratchable overlay view
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.red)
-                .frame(width: 250, height: 250)
+        VStack {
+            Image("pokemon-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250)
 
-            // MARK: Hidden content view
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.yellow)
-                .frame(width: 250, height: 250)
-                .overlay {
-                    Image("money-bag", bundle: nil)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100)
-                }
-                .mask(
-                    Path { path in
-                        path.addLines(points)
-                    }.stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round))
-                )
-                .gesture(
-                    DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                        .onChanged({ value in
-                            points.append(value.location)
-                        })
-                )
+            ZStack {
+                Image("pokemon-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250)
+
+                // MARK: Scratchable overlay view
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.red)
+                    .frame(width: 250, height: 250)
+                    .overlay {
+                        Image("pokeball")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                    }
+
+                // MARK: Hidden content view
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(.yellow)
+                    .frame(width: 250, height: 250)
+                    .overlay {
+                        Image("pikachu")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150)
+                    }
+                    .mask(
+                        Path { path in
+                            path.addLines(points)
+                        }.stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round))
+                    )
+                    .gesture(
+                        DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                            .onChanged({ value in
+                                points.append(value.location)
+                            })
+                    )
+            }
         }
+        
     }
 }
 
