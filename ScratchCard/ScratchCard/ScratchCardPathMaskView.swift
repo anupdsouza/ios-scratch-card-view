c016d1766edaa407bf85bebd7b9d164f0eaf3b8c
Anup D'Souza <anup.dsouza@gmail.com>
2024-08-30 16:20:52 +0530

    Update dataset & add shine animation

--- ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
+++ ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
@@ -14,6 +14,13 @@ import SwiftUI
 
 struct ScratchCardPathMaskView: View {
     @State var points = [CGPoint]()
+    @State var selection: Int = 0
+    @State var shine = true
+    let pokemon:[[String: Any]] = [
+        ["name":"pikachu-3d", "color": Color.yellow],
+        ["name":"squirtle-3d", "color": Color.cyan],
+        ["name":"bulbasaur-3d", "color": Color.mint]
+    ]
     
     var body: some View {
         VStack {
@@ -33,16 +40,28 @@ struct ScratchCardPathMaskView: View {
                             .scaledToFit()
                             .frame(width: 150)
                     }
+                    .overlay {
+                        LinearGradient(colors: [.clear, .white.opacity(0.8), .clear], startPoint: .leading, endPoint: .trailing)
+                            .frame(width: 100)
+                            .offset(x: shine ? -250 : 250)
+                            .rotationEffect(.degrees(-45))
+                            .scaleEffect(2)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .animation(.linear(duration: 3.0).repeatForever(autoreverses: false), value: shine)
+                    .onAppear(perform: {
+                        shine.toggle()
+                    })
 
                 // MARK: Hidden content view
                 RoundedRectangle(cornerRadius: 20)
-                    .fill(.yellow)
+                    .fill(pokemon[selection]["color"] as? Color ?? .yellow)
                     .frame(width: 250, height: 250)
                     .overlay {
-                        Image("pikachu")
+                        Image(pokemon[selection]["name"] as? String ?? "pikachu-3d")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 150)
+                            .frame(width: 200)
                     }
                     .mask(
                         Path { path in
@@ -56,8 +75,25 @@ struct ScratchCardPathMaskView: View {
                             })
                     )
             }
+            
+            Button(action: {
+                selection = (selection + 1) % pokemon.count
+            }, label: {
+                Text("Catch New !")
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .frame(width: 220)
+                    .padding(.vertical, 8)
+            })
+            .buttonStyle(.borderedProminent)
+            .tint(.red)
+            .clipShape(Capsule())
+            .padding(.vertical, 20)
         }
-        
+        .onChange(of: selection) {
+            points = []
+        }
     }
 }
 
