f9d00278a8687b74e8c798c98f0217330d25b26a
Anup D'Souza <anup.dsouza@gmail.com>
2024-08-31 20:30:50 +0530

    Add shine effect & prepare for scratch amount detection

--- ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
+++ ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
@@ -13,10 +13,13 @@
 import SwiftUI
 
 struct ScratchCardPathMaskView: View {
-    @State var points = [CGPoint]()
-    @State var selection: Int = 0
-    @State var shine = true
-    let pokemon:[[String: Any]] = [
+    @State private var points = [CGPoint]()
+    @State private var selection: Int = 0
+    @State private var shine = true
+    @State private var clearScratchArea = false
+
+    private let scratchClearAmount: CGFloat = 0.5
+    private let pokemon: [[String: Any]] = [
         ["name":"pikachu-3d", "color": Color.yellow],
         ["name":"squirtle-3d", "color": Color.cyan],
         ["name":"bulbasaur-3d", "color": Color.mint]
@@ -40,6 +43,8 @@ struct ScratchCardPathMaskView: View {
                             .scaledToFit()
                             .frame(width: 150)
                     }
+                    .opacity(clearScratchArea ? 0 : 1)
+                    .animation(.easeInOut, value: clearScratchArea)
                     .overlay {
                         LinearGradient(colors: [.clear, .white.opacity(0.8), .clear], startPoint: .leading, endPoint: .trailing)
                             .frame(width: 100)
@@ -49,31 +54,45 @@ struct ScratchCardPathMaskView: View {
                     }
                     .clipShape(RoundedRectangle(cornerRadius: 20))
                     .animation(.linear(duration: 3.0).repeatForever(autoreverses: false), value: shine)
+                    .shadow(color: .black, radius: 2)
                     .onAppear(perform: {
                         shine.toggle()
                     })
 
-                // MARK: Hidden content view
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(pokemon[selection]["color"] as? Color ?? .yellow)
-                    .frame(width: 250, height: 250)
-                    .overlay {
-                        Image(pokemon[selection]["name"] as? String ?? "pikachu-3d")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 200)
-                    }
-                    .mask(
-                        Path { path in
-                            path.addLines(points)
-                        }.stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round))
-                    )
-                    .gesture(
-                        DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                            .onChanged({ value in
-                                points.append(value.location)
-                            })
-                    )
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(pokemon[selection]["color"] as? Color ?? .yellow)
+                        .frame(width: 250, height: 250)
+                        .shadow(color: .black, radius: 2)
+                        .overlay {
+                            Image(pokemon[selection]["name"] as? String ?? "pikachu-3d")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 200)
+                        }
+                        .opacity(clearScratchArea ? 1 : 0)
+
+                    // MARK: Hidden content view
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(pokemon[selection]["color"] as? Color ?? .yellow)
+                        .frame(width: 250, height: 250)
+                        .overlay {
+                            Image(pokemon[selection]["name"] as? String ?? "pikachu-3d")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 200)
+                        }
+                        .mask(
+                            Path { path in
+                                path.addLines(points)
+                            }.stroke(style: StrokeStyle(lineWidth: 50, lineCap: .round, lineJoin: .round))
+                        )
+                        .gesture(
+                            DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                                .onChanged({ value in
+                                    points.append(value.location)
+                                })
+                        )
+                        .opacity(clearScratchArea ? 0 : 1)
             }
             
             Button(action: {
@@ -93,6 +112,7 @@ struct ScratchCardPathMaskView: View {
         }
         .onChange(of: selection) {
             points = []
+            clearScratchArea = false
         }
     }
 }
