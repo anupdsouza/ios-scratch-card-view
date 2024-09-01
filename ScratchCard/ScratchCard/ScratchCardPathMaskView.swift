20f5e63cb660cf2d0ec3b27784096b7d5955ccb6
Anup D'Souza <anup.dsouza@gmail.com>
2024-09-01 15:29:25 +0530

    Create modifier for shine effect

--- ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
+++ ScratchCard/ScratchCard/ScratchCardPathMaskView.swift
@@ -15,7 +15,7 @@ import SwiftUI
 struct ScratchCardPathMaskView: View {
     @State private var points = [CGPoint]()
     @State private var selection: Int = 0
-    @State private var shine = true
+    @State private var topViewShine = true
     @State private var clearScratchArea = false
 
     private let scratchClearAmount: CGFloat = 0.5
@@ -33,7 +33,7 @@ struct ScratchCardPathMaskView: View {
                 .frame(width: 250)
 
             ZStack {
-                // MARK: Scratchable overlay view
+                // MARK: Scratchable TOP view
                 RoundedRectangle(cornerRadius: 20)
                     .fill(.red)
                     .frame(width: 250, height: 250)
@@ -45,18 +45,12 @@ struct ScratchCardPathMaskView: View {
                     }
                     .opacity(clearScratchArea ? 0 : 1)
                     .animation(.easeInOut, value: clearScratchArea)
-                    .overlay {
-                        LinearGradient(colors: [.clear, .white.opacity(0.8), .clear], startPoint: .leading, endPoint: .trailing)
-                            .frame(width: 100)
-                            .offset(x: shine ? -250 : 250)
-                            .rotationEffect(.degrees(-45))
-                            .scaleEffect(2)
-                    }
+                    .shimmer(shine: $topViewShine)
                     .clipShape(RoundedRectangle(cornerRadius: 20))
-                    .animation(.linear(duration: 3.0).repeatForever(autoreverses: false), value: shine)
+                    .compositingGroup()
                     .shadow(color: .black, radius: 2)
                     .onAppear(perform: {
-                        shine.toggle()
+                        topViewShine.toggle()
                     })
 
                     RoundedRectangle(cornerRadius: 20)
@@ -71,7 +65,7 @@ struct ScratchCardPathMaskView: View {
                         }
                         .opacity(clearScratchArea ? 1 : 0)
 
-                    // MARK: Hidden content view
+                    // MARK: Hidden REVEAL view
                     RoundedRectangle(cornerRadius: 20)
                         .fill(pokemon[selection]["color"] as? Color ?? .yellow)
                         .frame(width: 250, height: 250)
@@ -112,13 +106,43 @@ struct ScratchCardPathMaskView: View {
             .clipShape(Capsule())
             .padding(.vertical, 20)
         }
-        .onChange(of: selection) {
+        .onChange(of: selection, perform: { value in
             points = []
             clearScratchArea = false
-        }
+        })
     }
 }
 
 #Preview {
     ScratchCardPathMaskView()
+}
+
+struct ShimmerModifier: ViewModifier {
+    var repeatCount: Int? = nil
+    var duration: Double
+    
+    @Binding var shine: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                LinearGradient(colors: [.clear, .white.opacity(0.8), .clear], startPoint: .leading, endPoint: .trailing)
+                    .frame(width: 100)
+                    .offset(x: shine ? -250 : 250)
+                    .rotationEffect(.degrees(-45))
+                    .scaleEffect(2)
+                    .animation(
+                        repeatCount != nil ?
+                            .linear(duration: duration).repeatCount(repeatCount!, autoreverses: false) :
+                                .linear(duration: duration).repeatForever(autoreverses: false),
+                        value: shine
+                    )
+            )
+    }
+}
+
+extension View {
+    func shimmer(repeatCount: Int? = nil, duration: Double = 3.0, shine: Binding<Bool>) -> some View {
+        self.modifier(ShimmerModifier(repeatCount: repeatCount, duration: duration, shine: shine))
+    }
 }
